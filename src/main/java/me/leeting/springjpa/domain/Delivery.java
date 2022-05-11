cd7b5cd0916bad0d6369b10cc4ee9da8bad0b6f6
leetingman <dwa08@naver.com>
2022-05-11 14:38:22 +0900

    change fetch type EAGER to LAZY

--- src/main/java/me/leeting/springjpa/domain/Delivery.java
+++ src/main/java/me/leeting/springjpa/domain/Delivery.java
@@ -5,13 +5,15 @@ import lombok.Setter;
 
 import javax.persistence.*;
 
+import static javax.persistence.FetchType.LAZY;
+
 @Entity
 @Getter @Setter
 public class Delivery {
     @Id @GeneratedValue
     @Column(name = "delivery_id")
     private Long id;
-    @OneToOne(mappedBy = "delivery")
+    @OneToOne(mappedBy = "delivery",fetch = LAZY)
     private Order order;
     @Embedded
     private Address address;
