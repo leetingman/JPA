a3222ca57119aaa51a19370af637a7cae572877d
leetingman <dwa08@naver.com>
2022-05-12 09:32:23 +0900

    Test error fix

--- src/main/java/me/leeting/springjpa/MemberRepository.java
+++ src/main/java/me/leeting/springjpa/MemberRepository.java
@@ -1,5 +1,6 @@
 package me.leeting.springjpa;
 
+import me.leeting.springjpa.domain.Member;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
--- src/test/java/me/leeting/springjpa/MemberRepositoryTest.java
+++ src/test/java/me/leeting/springjpa/MemberRepositoryTest.java
@@ -1,6 +1,7 @@
 package me.leeting.springjpa;
 
 import me.leeting.springjpa.commons.TestDescription;
+import me.leeting.springjpa.domain.Member;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
