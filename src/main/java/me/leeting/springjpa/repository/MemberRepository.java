fd812d6dde6b2f4d4f1f5d61ab493be8ff78d8aa
leetingman <dwa08@naver.com>
2022-05-12 13:47:14 +0900

    Create MemberRepository

---
+++ src/main/java/me/leeting/springjpa/repository/MemberRepository.java
@@ -0,0 +1,40 @@
+package me.leeting.springjpa.repository;
+
+import me.leeting.springjpa.domain.Member;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em;//injection
+
+    public void save(Member member){
+        em.persist(member);
+    }
+
+    public Member findOne(Long id){
+        return em.find(Member.class,id);
+    }
+
+    public List<Member> findAll(){
+        String jpql = "select m from Member m";
+        List<Member> result = em.createQuery(jpql, Member.class)//Jpql find all query (query,class)
+                .getResultList();
+        return result;
+    }
+
+    public List<Member> findByName(String name){
+        return em.createQuery("select m from Member m where m.name = :name",Member.class)
+                .setParameter("name",name)
+                .getResultList();
+
+    }
+
+
+
+}
