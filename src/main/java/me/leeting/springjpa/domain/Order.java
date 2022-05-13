50a3c61c8fb22765384d6819aa44e633825ba278
leetingman <dwa08@naver.com>
2022-05-13 16:02:57 +0900

    Create Order Entity

--- src/main/java/me/leeting/springjpa/domain/Order.java
+++ src/main/java/me/leeting/springjpa/domain/Order.java
@@ -11,22 +11,24 @@ import java.util.List;
 import static javax.persistence.FetchType.LAZY;
 
 @Entity
-@Table(name="orders")
-@Getter @Setter
+@Table(name = "orders")
+@Getter
+@Setter
 public class Order {
-    @Id @GeneratedValue
-    @Column(name="order_id")
+    @Id
+    @GeneratedValue
+    @Column(name = "order_id")
     private Long id;
 
     @ManyToOne(fetch = LAZY)
-    @JoinColumn(name="Member_id")
+    @JoinColumn(name = "Member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order" , cascade = CascadeType.ALL)
-    private List<OrderItem> orderItems =new ArrayList<>();
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne(fetch = LAZY ,cascade =CascadeType.ALL)
-    @JoinColumn(name= "delivery_id")
+    @OneToOne(fetch = LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
     private LocalDateTime orderDate;
@@ -35,23 +37,50 @@ public class Order {
     private OrderStatus status;
 
     //연관관계 메서드 setMember,addOrderItem,setDelivery
-    private void setMember(Member member){
+    private void setMember(Member member) {
         //Member -> List orders()
-        this.member=member;
+        this.member = member;
         member.getOrders().add(this);
     }
 
-    private void addOrderItem(OrderItem orderItem){
+    private void addOrderItem(OrderItem orderItem) {
         orderItems.add(orderItem);
         orderItem.setOrder(this);
     }
 
-    private void setDelivery(Delivery delivery)
-    {
-        this.delivery=delivery;
+    private void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
         delivery.setOrder(this);
     }
 
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for (OrderItem orederItem : orderItems) {
+            order.addOrderItem(orederItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        // java current time
+        order.setOrderDate(LocalDateTime.now());
+
+        return order;
+    }
+
+    public void cancel() {
+        if (delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("can not cancel");
+        }
+        this.setStatus(OrderStatus.CANCEL);
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel();
+        }
+    }
+
+    public int getTotalPrice() {
+        int totalPrice = orderItems.stream().mapToInt(OrderItem::getTotalPrice).sum();
+        return totalPrice;
+    }
 
 
 }
--- src/main/java/me/leeting/springjpa/domain/OrderItem.java
+++ src/main/java/me/leeting/springjpa/domain/OrderItem.java
@@ -23,4 +23,20 @@ public class OrderItem {
 
     private int orderPrice;
     private int count;
+
+    public static OrderItem createOrderItem(Item item ,int orderPrice,int count){
+        OrderItem orderItem=new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);//아이템 재고 수량 생성시 재고수량 감소
+       return orderItem;
+    }
+    public void cancel() {
+        getItem().addStock(count);//재고구 수량 복9
+    }
+    public int getTotalPrice() {
+        return getOrderPrice()*getCount();
+    }
 }
