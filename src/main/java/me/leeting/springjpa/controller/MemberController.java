d3174aa8455dec13e42c9d2b1e944a8343c25ae3
leetingman <dwa08@naver.com>
2022-05-19 15:24:45 +0900

    Add MemberController findMemberList

--- src/main/java/me/leeting/springjpa/controller/MemberController.java
+++ src/main/java/me/leeting/springjpa/controller/MemberController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -37,6 +38,12 @@ public class MemberController {
 
         memberService.join(member);
         return "redirect/";
+    }
+
+    @GetMapping("/members")
+    public String findMemberList(Model model){
+        model.addAttribute("members", memberService.findMembers());
+        return "members/memberList";
     }
 
 
