3df119f8c84a2c51b5be511d28dfb6ddfbb0ee83
leetingman <dwa08@naver.com>
2022-05-21 14:17:09 +0900

    dirty checking

--- src/main/java/me/leeting/springjpa/service/ItemService.java
+++ src/main/java/me/leeting/springjpa/service/ItemService.java
@@ -2,6 +2,7 @@ package me.leeting.springjpa.service;
 
 
 import lombok.RequiredArgsConstructor;
+import me.leeting.springjpa.domain.item.Book;
 import me.leeting.springjpa.domain.item.Item;
 import me.leeting.springjpa.repository.ItemRepository;
 import org.springframework.stereotype.Service;
@@ -18,6 +19,24 @@ public class ItemService {
     @Transactional
     public void saveItem(Item item){
         itemRepository.save(item);
+    }
+
+    /**
+     * spring Transactional 에 의해 commit 된디
+     * jpa 는 flush 를 날림
+     * 트랜잭션 커밋시점에 영속성 엔티티 에서 변경된 값을 찾음
+     * 더티checking 권장 사항
+     * @param itemId
+     * @param param
+     */
+    @Transactional
+    public void updateItem(Long itemId,String name,int price ,int stockQuantity){
+        Item findItem = itemRepository.findOne(itemId);//영속성 상
+        findItem.setPrice(price);
+        findItem.setName(name);
+        findItem.setStockQuantity(stockQuantity);
+
+        //update 해야될것 이 많으면 Dto 생성
     }
 
     public List<Item> findItems(){
---
+++ src/main/java/me/leeting/springjpa/service/UpdateItemDto.java
@@ -0,0 +1,12 @@
+package me.leeting.springjpa.service;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class UpdateItemDto {
+
+    private String name;
+    private int price ;
+    private int stockQuantity;
+}
