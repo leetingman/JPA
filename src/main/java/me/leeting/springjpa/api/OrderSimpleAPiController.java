54cce2960bd8bdaa717df1bd05b75340dc08b21d
leetingman <dwa08@naver.com>
2022-07-05 15:39:53 +0900

    order API ver2

--- src/main/java/me/leeting/springjpa/api/OrderSimpleAPiController.java
+++ src/main/java/me/leeting/springjpa/api/OrderSimpleAPiController.java
@@ -1,14 +1,19 @@
 package me.leeting.springjpa.api;
 
 
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import me.leeting.springjpa.domain.Address;
 import me.leeting.springjpa.domain.Order;
+import me.leeting.springjpa.domain.OrderStatus;
 import me.leeting.springjpa.repository.OrderRepository;
 import me.leeting.springjpa.repository.OrderSearch;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * x TO ONE
@@ -31,6 +36,33 @@ public class OrderSimpleAPiController {
         }
         return all;
         //Hibernate5
+    }
+
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2(){
+        List<Order> orders= orderRepository.findAllByString(new OrderSearch());
+
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(Collectors.toList());
+        return result;
+    }
+
+    @Data
+    static class SimpleOrderDto{
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order){
+            orderId= order.getId();
+            name=order.getMember().getName();
+            orderDate=order.getOrderDate();
+            orderStatus=order.getStatus();
+            address= order.getDelivery().getAddress();
+        }
     }
 
 }
