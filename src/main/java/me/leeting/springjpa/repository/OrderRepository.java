3f19c5d556981a7ab551a49c0bb88697a16d0436
leetingman <dwa08@naver.com>
2022-07-07 15:59:49 +0900

    orders api fetch join

--- src/main/java/me/leeting/springjpa/repository/OrderRepository.java
+++ src/main/java/me/leeting/springjpa/repository/OrderRepository.java
@@ -107,4 +107,15 @@ public class OrderRepository {
         return result;
 
     }
+
+    public List<Order> findAllwithItem() {
+        return em.createQuery(
+                "select distinct o from Order o"+
+                        " join fetch o.member m"+
+                        " join fetch o.delivery d"+
+                        " join fetch o.orderItems oi"+
+                        " join fetch oi.item i",Order.class
+        ).getResultList();
+
+    }
 }
